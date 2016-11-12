6815ba044d362b3b4dc281cd25234bfcc2e3525d
Sam <phreakdawg@gmail.com>
2016-11-11 18:46:28 -0700

    Adds ability to pass album in querystring

--- js/scripts/MainCtrl.js
+++ js/scripts/MainCtrl.js
@@ -1,18 +1,26 @@
-(function() {
-	var app = angular.module('ImgurAlbum')
-    .controller('MainCtrl', ['$scope', 'imgursvc', MainCtrl]);
+(function () {
+    var app = angular.module('ImgurAlbum')
+        .controller('MainCtrl', ['$scope', '$location', 'imgursvc', MainCtrl]);
 
-	function MainCtrl($scope, imgursvc) {
-		var album = {};
+    function MainCtrl($scope, $location, imgursvc) {
+        var album = {};
+
+        // example:  file:///C:/Users/Sam/Documents/GitHub/ImgurAlbumThumbs/index.html#/!/?albumName=xxx
+        var albumName = $location.search().albumName;
+        if (albumName !== undefined) {
+            imgursvc.getAlbumImages(albumName).then(function (data) {
+                album.images = data;
+            });
+        }
 
         album.submit = function () {
-         imgursvc.getAlbumImages(album.code).then(function(data) {
-             album.images = data;
-         });
+            imgursvc.getAlbumImages(album.code).then(function (data) {
+                album.images = data;
+            });
         };
 
-		album.msg = "Welcome to the Imgur Album program!";
+        album.msg = "Welcome to the Imgur Album program!";
 
         $scope.album = album;
-	};
+    };
 })();
